800148852b196edbca04222ad9d6131d9a3b97ed
Korneliusz Pogorzelczyk <kor.pogorzelczyk@gmail.com>
2024-01-03 11:11:22 +0100

    added templates

---
+++ django_sent/sentiment/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+app_name = "sentiment"
+urlpatterns = [
+    path("", views.index, name="index"),
+]
--- django_sent/sentiment/views.py
+++ django_sent/sentiment/views.py
@@ -1,6 +1,6 @@
 import os
 import torch
-from lstm import LSTM_NN
+from . import lstm
 from django.shortcuts import render
 from django_sent.settings import STATIC_ROOT
 
@@ -12,9 +12,13 @@ hidden_dim = 128
 
 PATH = os.path.join(STATIC_ROOT, "sentiment/model_state_dict.pt")
 
-model = LSTM_NN(no_layers, vocab_size, hidden_dim,
+model = lstm.LSTM_NN(no_layers, vocab_size, hidden_dim,
                 embedding_dim, drop_prob=0.5)
 model.load_state_dict(torch.load(PATH))
 model.eval()
+
+def index(request):
+    return render(request, "sentiment/index.html")
+
 
 
